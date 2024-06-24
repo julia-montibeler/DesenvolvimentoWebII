414110c5ffd84d2546258fb411def52ebd5cdb4e
Kuglin <kuglin2002@gmail.com>
2024-06-24 17:53:03 -0300

    sistema de login

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-import json
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, json, render_template, request, redirect, url_for, jsonify, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import requests
 
-from models.pokemon import Pokemon
-
 app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 def fetch_pokemon_data(id):
     url = f"https://pokeapi.co/api/v2/pokemon/{id}"
@@ -20,16 +33,14 @@ def fetch_pokemon_data(id):
 @app.route('/')
 def home():
     silhouette = "https://i.pinimg.com/originals/3d/f6/ef/3df6eff48175fa05ec90a00415fcfe25.png"
-    return render_template('home.html', nome = "??", foto = silhouette, peso = 0, altura = 0, tipos = "??", id = 0, foto_costas = silhouette, foto_shiny = silhouette, foto_costas_shiny = silhouette)
+    return render_template('home.html', nome="??", foto=silhouette, peso=0, altura=0, tipos="??", id=0, foto_costas=silhouette, foto_shiny=silhouette, foto_costas_shiny=silhouette)
 
-@app.route('/buscar', methods = ["GET", "POST"])
+@app.route('/buscar', methods=["GET", "POST"])
+@login_required
 def buscar():
     pokemon = request.form["nome"].lower()
     try:
         res = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").text)
-
-        print(res["forms"][0]["name"].title())
-
         foto = res["sprites"]["front_default"]
         foto_costas = res["sprites"]["back_default"]
         foto_shiny = res["sprites"]["front_shiny"]
@@ -39,16 +50,15 @@ def buscar():
         peso = res["weight"]
         altura = res["height"]
         tipos = [res["types"][0]["type"]["name"].title()]
-        if len(res["types"])> 1:
+        if len(res["types"]) > 1:
             tipos.append(" e " + res["types"][1]["type"]["name"].title())
-          
     except:
         silhouette = "https://i.pinimg.com/originals/3d/f6/ef/3df6eff48175fa05ec90a00415fcfe25.png"
-        return render_template('home.html', nome = "??", foto = silhouette, peso = 0, altura = 0, tipos = "??", id = 0, foto_costas = silhouette, foto_shiny = silhouette, foto_costas_shiny = silhouette)
-    
-    return render_template('home.html', nome = nome, foto = foto, peso = peso, altura = altura, tipos = tipos, id = id, foto_costas = foto_costas, foto_shiny = foto_shiny, foto_costas_shiny = foto_costas_shiny)
+        return render_template('home.html', nome="??", foto=silhouette, peso=0, altura=0, tipos="??", id=0, foto_costas=silhouette, foto_shiny=silhouette, foto_costas_shiny=silhouette)
+    return render_template('home.html', nome=nome, foto=foto, peso=peso, altura=altura, tipos=tipos, id=id, foto_costas=foto_costas, foto_shiny=foto_shiny, foto_costas_shiny=foto_costas_shiny)
 
 @app.route('/pokedex')
+@login_required
 def pokedex():
     pokemon_list = [" ".join(i["name"].split("-")).title() for i in requests.get(f'https://pokeapi.co/api/v2/pokemon/?limit=-1').json()["results"]]
     return render_template("pokedex.html", pokemon=pokemon_list)
@@ -60,5 +70,41 @@ def pokemon_detail(id):
         return jsonify(pokemon_data)
     return jsonify({'error': 'Pokémon não encontrado'}), 404
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists', 'danger')
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created successfully', 'success')
+            return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
